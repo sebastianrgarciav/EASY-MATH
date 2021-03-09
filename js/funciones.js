260cc2841f14e9a5da2b7642205ea97d5008b22b
Sebastian Rodrigo <76745985+sebstianrodrigo@users.noreply.github.com>
2021-03-09 14:07:37 -0500

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,42 +1,114 @@
 function metros(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = x*1000;
+    var valor = x*1000;
+    document.getElementById('result').innerHTML = valor.toFixed(2);
     document.getElementById('unit').innerHTML = "m";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function kilometros(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = x/1000;
+    var valor = x/1000;
+    document.getElementById('result').innerHTML = valor;
     document.getElementById('unit').innerHTML = "Km";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function gramos(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = x*1000;
+    var valor = x*1000;
+    document.getElementById('result').innerHTML = valor.toFixed(2);
     document.getElementById('unit').innerHTML = "g";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function kilogramos(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = x/1000;
+    var valor = x/1000;
+    document.getElementById('result').innerHTML = valor.toFixed(2);
     document.getElementById('unit').innerHTML = "Kg";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function sexagesimales(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = (x*(180/Math.PI)).toFixed(3);
+    var valor = (x*(180/Math.PI));
+    document.getElementById('result').innerHTML = valor.toFixed(3);
     document.getElementById('unit').innerHTML = "";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function radianes(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = (x*(Math.PI/180)).toFixed(3);
+    var valor = (x*(Math.PI/180));
+    document.getElementById('result').innerHTML = valor.toFixed(3);
     document.getElementById('unit').innerHTML = "";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function fahrenheit(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = ((1.8*x)+32).toFixed(3);
+    var valor =  ((1.8*x)+32);
+    document.getElementById('result').innerHTML =valor.toFixed(3);
     document.getElementById('unit').innerHTML = "F";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function celcius(){
     var x =parseFloat(document.getElementById('value1').value);
-    document.getElementById('result').innerHTML = ((5/9)*(x-32)).toFixed(3);
+    var valor =((5/9)*(x-32));
+    document.getElementById('result').innerHTML =valor.toFixed(3);
     document.getElementById('unit').innerHTML = "°C";
+    if(valor >= 99999 || valor<= -99999){
+        document.getElementById('result').style.fontSize="22px";
+        document.getElementById('unit').style.fontSize="22px";
+    }
+    else{
+        document.getElementById('result').style.fontSize="32px";
+        document.getElementById('unit').style.fontSize="32px";
+    }
 }
 function inteSimple(){
     var x =parseFloat(document.getElementById('value1').value);
@@ -47,7 +119,7 @@ function inteSimple(){
     if(selectValue == "Meses"){
         var meses = (x*(y/100)*(z/12)).toFixed(2);
         document.getElementById('result').innerHTML = meses;
-        if(meses >= 99999){
+        if(meses >= 99999 || valor<= -99999){
             document.getElementById('result').style.fontSize="21px";
             document.getElementById('unit').style.fontSize="21px";
         }
@@ -59,7 +131,7 @@ function inteSimple(){
     else{
         year=(x*(y/100)*z).toFixed(2);
         document.getElementById('result').innerHTML = year;
-        if(meses >= 99999){
+        if(meses >= 99999 || valor<= -99999){
             document.getElementById('result').style.fontSize="21px";
             document.getElementById('unit').style.fontSize="21px";
         }
@@ -79,7 +151,7 @@ function inteCompuesto(){
     if(selectValue == "Meses"){
         var meses = (x*((1+y/100)**(z/12))).toFixed(2);
         document.getElementById('result').innerHTML = meses;
-        if(meses >= 99999){
+        if(meses >= 99999 || valor<= -99999){
             document.getElementById('result').style.fontSize="21px";
             document.getElementById('unit').style.fontSize="21px";
         }
@@ -92,7 +164,7 @@ function inteCompuesto(){
     else{
         var year = (x*((1+y/100)**z)).toFixed(2);
         document.getElementById('result').innerHTML = year;
-        if(year >= 99999){
+        if(year >= 99999 || valor<= -99999){
             document.getElementById('result').style.fontSize="21px";
             document.getElementById('unit').style.fontSize="21px";
         }
@@ -111,7 +183,7 @@ function inteTasa(){
     if(selectValue == "Meses"){
         var meses = ((y*100)/(x*(z/12))).toFixed(2);
         document.getElementById('result').innerHTML = meses;
-        if(meses >= 99999){
+        if(meses >= 99999 || valor<= -99999){
             document.getElementById('result').style.fontSize="21px";
             document.getElementById('unit').style.fontSize="21px";
         }
@@ -124,7 +196,7 @@ function inteTasa(){
     else{
         var year = ((y*100)/(x*(z))).toFixed(2);
         document.getElementById('result').innerHTML = year;
-        if(year >= 99999){
+        if(year >= 99999 || valor<= -99999){
             document.getElementById('result').style.fontSize="21px";
             document.getElementById('unit').style.fontSize="21px";
         }
@@ -141,7 +213,7 @@ function primerG(){
     document.getElementById('unit').innerHTML = "X:   ";
     var valor = ((-y)/x).toFixed(2);
     document.getElementById('result').innerHTML = valor;
-    if(valor >= 99999){
+    if(valor >= 99999 || valor<= -99999){
         document.getElementById('result').style.fontSize="22px";
         document.getElementById('unit').style.fontSize="22px";
     }
@@ -162,7 +234,7 @@ function segundoG(){
     var valor = ((-y)-((y)**2-(4*x*z))**0.5)/(2*x);
     document.getElementById('result2').innerHTML = valor.toFixed(3);
 
-    if(valor >= 99999){
+    if(valor >= 99999|| valor<= -99999){
         document.getElementById('result1').style.fontSize="21px";
         document.getElementById('unit1').style.fontSize="21px";
         document.getElementById('result2').style.fontSize="21px";
@@ -174,6 +246,39 @@ function segundoG(){
         document.getElementById('result2').style.fontSize="32px";
         document.getElementById('unit2').style.fontSize="32px";
     }
-
+}
+function dist(){
+    var x =parseFloat(document.getElementById('value1').value);
+    var y =parseFloat(document.getElementById('value2').value);
+    var z =parseFloat(document.getElementById('value3').value);
+    var w =parseFloat(document.getElementById('value4').value);
+    document.getElementById('unit1').innerHTML = "D = ";
+    var valor =((y-x)**2 + (w-z)**2)**0.5;
+    document.getElementById('result1').innerHTML = valor.toFixed(2);
+    if(valor >= 99999|| valor<= -99999){
+        document.getElementById('result1').style.fontSize="21px";
+        document.getElementById('unit1').style.fontSize="21px";
+    }
+    else{
+        document.getElementById('result1').style.fontSize="32px";
+        document.getElementById('unit1').style.fontSize="32px";
+    }
+}
+function pend(){
+    var x =parseFloat(document.getElementById('value1').value);
+    var y =parseFloat(document.getElementById('value2').value);
+    var z =parseFloat(document.getElementById('value3').value);
+    var w =parseFloat(document.getElementById('value4').value);
+    document.getElementById('unit1').innerHTML = "D = ";
+    var valor = ((w-z)/(y-x));
+    document.getElementById('result1').innerHTML = valor.toFixed(2);
+    if(valor >= 99999|| valor<= -99999){
+        document.getElementById('result1').style.fontSize="21px";
+        document.getElementById('unit1').style.fontSize="21px";
+    }
+    else{
+        document.getElementById('result1').style.fontSize="32px";
+        document.getElementById('unit1').style.fontSize="32px";
+    }
 }
 
